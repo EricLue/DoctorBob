040309a6be9c0747362aeb85c6964e9c2571d45a
Eric Luechinger <ericmarcel.luechinger.gmail.com@zbw-online.ch>
2022-01-26 22:18:18 +0100

    Moved DoctorBobContext to Common\Infrastructure\Context Created IRepository and IStaffRepository

---
+++ DoctorBob.Core/Common/Infrastructure/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorBob.Core.Common.Infrastructure
+{
+    interface IRepository
+    {
+    }
+}
