6b8b74dd73f27c36f13a9ccfe3cca2fd6923cc0b
Eric Luechinger <ericmarcel.luechinger.gmail.com@zbw-online.ch>
2022-01-26 23:18:31 +0100

    First try to get Entities from DoctorBob.Core

--- DoctorBob.UI/Startup.cs
+++ DoctorBob.UI/Startup.cs
@@ -23,6 +23,7 @@ namespace DoctorBob.UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IStaffRepository, StaffRepository>();
             services.AddRazorPages();
         }
 
