ffdabaf07db00cf81782f2ff0e0f205e1bc4c7d4
Eric Luechinger <ericmarcel.luechinger.gmail.com@zbw-online.ch>
2022-01-27 17:05:20 +0100

    First Setup Login Page

---
+++ DoctorBob.UI/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DoctorBob.UI.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+
+        public Credential Credential { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+
+        }
+    }
+
+    public class Credential
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
