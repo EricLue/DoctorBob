fd3556db2511c87d4eae00696ce8313c3c642f6b
Eric Luechinger <ericmarcel.luechinger.gmail.com@zbw-online.ch>
2022-05-13 18:57:26 +0100

    Updated Frontend Views and Added Order-Entity

---
+++ DoctorBob.Core/OrderManagement/Domain/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.ComponentModel.DataAnnotations.Schema;
+using DoctorBob.Core.RoboManagement.Domain;
+using DoctorBob.Core.PatientManagement.Domain;
+using DoctorBob.Core.Common.Domain;
+
+namespace DoctorBob.Core.OrderManagement.Domain
+{
+    class Order : AuditableEntity
+    {
+        public int Id { get; set; }
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RobotId { get; set; }
+        public Robot Robot { get; set; }
+        public List<Patient> Patients { get; set; }
+        public State State { get; set; }
+    }
+
+    public enum State
+    {
+        Boarding,
+        Confirmed,
+        Executing,
+        Completed
+    }
+}
