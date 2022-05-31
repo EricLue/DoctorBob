1574fcea1b3d5dbf8b527bcda1f26214f22e89c5
Eric Luechinger <ericmarcel.luechinger.gmail.com@zbw-online.ch>
2022-05-31 20:08:28 +0200

    MQTT erfolgreicher Versuch

---
+++ DoctorBob.Core/API/MQTTClient.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
+using MQTTnet.Client.Options;
+using MQTTnet.Extensions.ManagedClient;
+using Newtonsoft.Json;
+using Serilog;
+
+namespace DoctorBob.Core.API
+{
+    public class MQTTClient
+    {
+
+        public static string Main()
+        {
+            // Creates a new client
+            MqttClientOptionsBuilder builder = new MqttClientOptionsBuilder()
+                                                    .WithClientId("Dev.To")
+                                                    .WithTcpServer("10.100.201.21", 1883);
+
+            // Create client options objects
+            ManagedMqttClientOptions options = new ManagedMqttClientOptionsBuilder()
+                                    .WithAutoReconnectDelay(TimeSpan.FromSeconds(60))
+                                    .WithClientOptions(builder.Build())
+                                    .Build();
+
+            // Creates the client object
+            IManagedMqttClient _mqttClient = new MqttFactory().CreateManagedMqttClient();
+
+            // Set up handlers
+            _mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(OnConnected);
+            _mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(OnDisconnected);
+            _mqttClient.ConnectingFailedHandler = new ConnectingFailedHandlerDelegate(OnConnectingFailed);
+
+            // Starts a connection with the Broker
+            _mqttClient.StartAsync(options).GetAwaiter().GetResult();
+
+            // Send a new message to the broker every second
+
+            // MUSS IN FOREACH - FÜR JEDEN P
+            string patientId = "1001";
+            _mqttClient.PublishAsync("Raum/101/Patient/", patientId);
+            string drugId = "1";
+            _mqttClient.PublishAsync("Raum/101/Medikament/", drugId);
+
+            
+
+            Task.Delay(1000).GetAwaiter().GetResult();
+            
+            return "";
+        }
+
+        public static void OnConnected(MqttClientConnectedEventArgs obj)
+        {
+            Log.Logger.Information("Successfully connected.");
+        }
+
+        public static void OnConnectingFailed(ManagedProcessFailedEventArgs obj)
+        {
+            Log.Logger.Warning("Couldn't connect to broker.");
+        }
+
+        public static void OnDisconnected(MqttClientDisconnectedEventArgs obj)
+        {
+            Log.Logger.Information("Successfully disconnected.");
+        }
+    }
+}
