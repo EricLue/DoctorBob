4db630f7894520f14eb81ed2bb6338943aeeeb93
Eric Luechinger <ericmarcel.luechinger.gmail.com@zbw-online.ch>
2022-06-14 00:47:03 +0200

    Fixed MQTT-Call in separate Page

--- DoctorBob.Core/API/MQTTClient.cs
+++ DoctorBob.Core/API/MQTTClient.cs
@@ -18,41 +18,42 @@ namespace DoctorBob.Core.API
 
         public static string Main()
         {
-            // Creates a new client
-            MqttClientOptionsBuilder builder = new MqttClientOptionsBuilder()
-                                                    .WithClientId("Dev.To")
-                                                    .WithTcpServer("10.100.201.21", 1883);
-
-            // Create client options objects
-            ManagedMqttClientOptions options = new ManagedMqttClientOptionsBuilder()
-                                    .WithAutoReconnectDelay(TimeSpan.FromSeconds(60))
-                                    .WithClientOptions(builder.Build())
-                                    .Build();
-
-            // Creates the client object
-            IManagedMqttClient _mqttClient = new MqttFactory().CreateManagedMqttClient();
-
-            // Set up handlers
-            _mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(OnConnected);
-            _mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(OnDisconnected);
-            _mqttClient.ConnectingFailedHandler = new ConnectingFailedHandlerDelegate(OnConnectingFailed);
-
-            // Starts a connection with the Broker
-            _mqttClient.StartAsync(options).GetAwaiter().GetResult();
-
-            // Send a new message to the broker every second
-
-            // MUSS IN FOREACH - FÜR JEDEN P
-            string patientId = "1001";
-            _mqttClient.PublishAsync("Raum/101/Patient/", patientId);
-            string drugId = "1";
-            _mqttClient.PublishAsync("Raum/101/Medikament/", drugId);
-
-            
-
-            Task.Delay(1000).GetAwaiter().GetResult();
-            
             return "";
+            //// Creates a new client
+            //MqttClientOptionsBuilder builder = new MqttClientOptionsBuilder()
+            //                                        .WithClientId("Dev.To")
+            //                                        .WithTcpServer("10.100.201.21", 1883);
+
+            //// Create client options objects
+            //ManagedMqttClientOptions options = new ManagedMqttClientOptionsBuilder()
+            //                        .WithAutoReconnectDelay(TimeSpan.FromSeconds(60))
+            //                        .WithClientOptions(builder.Build())
+            //                        .Build();
+
+            //// Creates the client object
+            //IManagedMqttClient _mqttClient = new MqttFactory().CreateManagedMqttClient();
+
+            //// Set up handlers
+            //_mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(OnConnected);
+            //_mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(OnDisconnected);
+            //_mqttClient.ConnectingFailedHandler = new ConnectingFailedHandlerDelegate(OnConnectingFailed);
+
+            //// Starts a connection with the Broker
+            //_mqttClient.StartAsync(options).GetAwaiter().GetResult();
+
+            //// Send a new message to the broker every second
+
+            //// MUSS IN FOREACH - FÜR JEDEN P
+            //string patientId = "1001";
+            //_mqttClient.PublishAsync("Raum/101/Patient/", patientId);
+            //string drugId = "1";
+            //_mqttClient.PublishAsync("Raum/101/Medikament/", drugId);
+
+            
+
+            //Task.Delay(1000).GetAwaiter().GetResult();
+            
+            //return "";
         }
 
         public static void OnConnected(MqttClientConnectedEventArgs obj)
---
+++ DoctorBob.UI/Pages/OrderManager/Confirm.cshtml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DoctorBob.Core.Common.Infrastructure.Context;
+using DoctorBob.Core.OrderManagement.Domain;
+using DoctorBob.Core.API;
+using System.IO;
+
+namespace DoctorBob.UI.Pages.OrderManager
+{
+    public class ConfirmModel : PageModel
+    {
+        private readonly DoctorBob.Core.Common.Infrastructure.Context.DoctorBobContext _context;
+
+        public ConfirmModel(DoctorBob.Core.Common.Infrastructure.Context.DoctorBobContext context)
+        {
+            _context = context;
+            MQTTClient.Main();
+        }
+
+        [BindProperty]
+        public Order Order { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Order = await _context.Orders
+                .Include(p => p.Robot).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Order == null)
+            {
+                return NotFound();
+            }
+
+            Order.StateId = 2;
+            _context.Attach(Order).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(Order.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Page();
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.Id == id);
+        }
+
+        public String GetRobotName(int id)
+        {
+            return _context.Robots.Find(id).Name;
+        }
+    }
+}
