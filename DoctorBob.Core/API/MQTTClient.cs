82ee5a8ff9ab60437b87d76947377f0c2922729d
Eric Luechinger <ericmarcel.luechinger.gmail.com@zbw-online.ch>
2022-06-16 20:06:07 +0200

    Tests MQTT erfolgreich

--- DoctorBob.Core/API/MQTTClient.cs
+++ DoctorBob.Core/API/MQTTClient.cs
@@ -17,50 +17,30 @@ namespace DoctorBob.Core.API
     public class MQTTClient
     {
 
-        public static string Main(List<String> commandsList)
+        public static void Main(List<string> commandsList)
         {
-            return "";
+            // Creates a new client
+            MqttClientOptionsBuilder builder = new MqttClientOptionsBuilder()
+                                                    .WithClientId("Dev.To")
+                                                    .WithTcpServer("10.100.201.21", 1883);
+            // Create client options objects
+            ManagedMqttClientOptions options = new ManagedMqttClientOptionsBuilder()
+                                    .WithAutoReconnectDelay(TimeSpan.FromSeconds(60))
+                                    .WithClientOptions(builder.Build())
+                                    .Build();
+            // Creates the client object
+            IManagedMqttClient _mqttClient = new MqttFactory().CreateManagedMqttClient();
+            // Set up handlers
+            _mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(OnConnected);
+            _mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(OnDisconnected);
+            _mqttClient.ConnectingFailedHandler = new ConnectingFailedHandlerDelegate(OnConnectingFailed);
+            // Starts a connection with the Broker
+            _mqttClient.StartAsync(options).GetAwaiter().GetResult();
 
-            // Wieder aktivieren
-
-            //// Creates a new client
-            //MqttClientOptionsBuilder builder = new MqttClientOptionsBuilder()
-            //                                        .WithClientId("Dev.To")
-            //                                        .WithTcpServer("10.100.201.21", 1883);
-
-            //// Create client options objects
-            //ManagedMqttClientOptions options = new ManagedMqttClientOptionsBuilder()
-            //                        .WithAutoReconnectDelay(TimeSpan.FromSeconds(60))
-            //                        .WithClientOptions(builder.Build())
-            //                        .Build();
-
-            //// Creates the client object
-            //IManagedMqttClient _mqttClient = new MqttFactory().CreateManagedMqttClient();
-
-            //// Set up handlers
-            //_mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(OnConnected);
-            //_mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(OnDisconnected);
-            //_mqttClient.ConnectingFailedHandler = new ConnectingFailedHandlerDelegate(OnConnectingFailed);
-
-            //// Starts a connection with the Broker
-            //_mqttClient.StartAsync(options).GetAwaiter().GetResult();
-
-            //// Send a new message to the broker every second
-
-
-            //################// MUSS IN FOREACH - FÜR JEDEN PATIENTEN / JEDEN COMMAND SENDEN
-
-            //string patientId = "1001";
-            //_mqttClient.PublishAsync("Raum/101/Patient/", patientId);
-            //string drugId = "1";
-            //_mqttClient.PublishAsync("Raum/101/Medikament/", drugId);
-
-
-            //#### KANN DEAKTIVIERT BLEIBEN
-
-            //Task.Delay(1000).GetAwaiter().GetResult();
-            //####
-            //return "";
+            foreach (string command in commandsList)
+            {
+                _mqttClient.PublishAsync(command);
+            }
         }
 
         public static void OnConnected(MqttClientConnectedEventArgs obj)
